c7fec182489781e59c6abee85e10ff2429ad29ed
JonatanAtadia <jonatan.atadia@gmail.com>
2023-12-21 11:21:25 +0100

    moveSnake, drawSnake, changeDirection

--- index.js
+++ index.js
@@ -66,9 +66,61 @@ function drawFood() {
   context.fillStyle = foodColor;
   context.fillRect(foodX, foodY, unitSize, unitSize);
 }
-function moveSnake() {}
-function drawSnake() {}
-function changeDirection() {}
+function moveSnake() {
+  const head = { x: snake[0].x + xVelocity, y: snake[0].y + yVelocity };
+
+  snake.unshift(head);
+  // if food is eaten
+  if (snake[0].x == foodX && snake[0].y == foodY) {
+    score += 1;
+    scoreText.textContent = score;
+    createFood();
+  } else {
+    snake.pop();
+  }
+}
+function drawSnake() {
+  context.fillStyle = snakeColor;
+  context.strokeStyle = snakeBorder;
+  snake.forEach((snakePart) => {
+    context.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
+    context.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
+  });
+}
+function changeDirection(event) {
+  const keyPressed = event.keyCode;
+  const LEFT = 37;
+  const UP = 38;
+  const RIGHT = 39;
+  const DOWN = 40;
+
+  const goingUp = yVelocity == -unitSize;
+  const goingDown = yVelocity == unitSize;
+  const goingRight = xVelocity == unitSize;
+  const goingLeft = xVelocity == -unitSize;
+
+  switch (true) {
+    case keyPressed == LEFT && !goingRight:
+      xVelocity = -unitSize;
+      yVelocity = 0;
+      break;
+
+    case keyPressed == UP && !goingDown:
+      xVelocity = 0;
+      yVelocity = -unitSize;
+      break;
+
+    case keyPressed == RIGHT && !goingLeft:
+      xVelocity = unitSize;
+      yVelocity = 0;
+      break;
+
+    case keyPressed == DOWN && !goingUp:
+      xVelocity = 0;
+      yVelocity = unitSize;
+      break;
+  }
+}
 function checkGameOver() {}
 function displayGameOver() {}
 function resetGame() {}
